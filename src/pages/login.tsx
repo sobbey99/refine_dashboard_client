f589b8df44e56b11d07444ec5752529b0446a723
sobbey99 <sobbey99@gmail.com>
2023-03-08 01:14:41 +0200

    Added oAUTH client id from console-google

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,8 @@ import { useEffect, useRef } from "react";
 import { useLogin } from "@pankod/refine-core";
 import { Container, Box } from "@pankod/refine-mui";
 
+import {yariga} from '../assets'
+
 import { CredentialResponse } from "../interfaces/google";
 
 export const Login: React.FC = () => {
@@ -18,7 +20,7 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: "popup",
-          client_id: "your-client-id",
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
