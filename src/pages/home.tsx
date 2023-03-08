34ce18cbe3667e1032c9e7e61b84021e3b6c5de2
sobbey99 <sobbey99@gmail.com>
2023-03-08 02:30:59 +0200

    PieChart component ReactApexChart

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,7 +12,36 @@ import {
 const home = () => {
   return (
     <Box>
-      
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
+
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart 
+          title="Proprties for Sale"
+          value={684}
+          series={[75, 25]}
+          colors={["#475be8", "#e3e8ef"]}
+        />
+        <PieChart 
+          title="Proprties for Rent"
+          value={550}
+          series={[60, 40]}
+          colors={["#475ae8", "#e4b8ef"]}
+        />
+        <PieChart 
+          title="Total customers"
+          value={5684}
+          series={[75, 25]}
+          colors={["#275be8", "#c4e8ef"]}
+        />
+        <PieChart 
+          title="Proprties for Cities"
+          value={555}
+          series={[75, 25]}
+          colors={["#475be8", "#e3e8ef"]}
+        />
+      </Box>
     </Box>
   )
 }
