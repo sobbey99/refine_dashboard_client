428b082476d1f22b1da21b26bcc8b93c52cec61d
sobbey99 <sobbey99@gmail.com>
2023-03-08 11:59:05 +0200

    When logging Added Function for sending request to mongoDb for creating a new user in DB

--- src/App.tsx
+++ src/App.tsx
@@ -54,21 +54,38 @@ axiosInstance.interceptors.request.use((request: AxiosRequestConfig) => {
 
 function App() {
   const authProvider: AuthProvider = {
-    login: ({ credential }: CredentialResponse) => {
+    login: async ({ credential }: CredentialResponse) => {
       const profileObj = credential ? parseJwt(credential) : null;
 
+
+      //Save user to MongoDB...
       if (profileObj) {
-        localStorage.setItem(
-          "user",
-          JSON.stringify({
-            ...profileObj,
-            avatar: profileObj.picture,
+        const response = await fetch("http://localhost:8080/api/v1/users", {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify({
+            name: profileObj.name,
+            email: profileObj.email,
+            avatar: profileObj.picture
           })
-        );
+        })
+
+        const data = await response.json()
+
+        if(response.status === 200) {
+          localStorage.setItem( 
+            "user",
+            JSON.stringify({
+              ...profileObj,
+              avatar: profileObj.picture,
+              userId: data._id
+            })
+          );
+        } else {
+          return Promise.reject()
+        }
       }
-
       localStorage.setItem("token", `${credential}`);
-
       return Promise.resolve();
     },
     logout: () => {
