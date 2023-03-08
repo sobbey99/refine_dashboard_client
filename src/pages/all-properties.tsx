c98fae30cd3b8f78fd7e960b271a31022e3c943d
sobbey99 <sobbey99@gmail.com>
2023-03-08 11:07:21 +0200

    added CreateProperty - Form

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import {Add} from "@mui/icons-material"
 
-const allProperties = () => {
+import { useList } from "@pankod/refine-core/dist/hooks"
+
+import { Box, Typography, Stack} from "@pankod/refine-mui"
+
+import { useNavigate } from "@pankod/refine-react-router-v6"
+
+import { PropertyCard, CustomButton } from "components"
+
+
+const AllProperties = () => {
+  const navigate = useNavigate()
+
   return (
-    <div>all-properties</div>
+    <Box>
+      <Stack direction="row" 
+      justifyContent="space-between"
+      alignItems="center">
+        <Typography fontSize={25} fontWeight={700} color="#11142d">All Properties</Typography>
+        <CustomButton 
+          title="Add Property"
+          handleClick={() => navigate(
+            "/properties/create"
+          )}
+          backgroundColor="#475be8"
+          color="#fcfcfc"
+          icon={<Add/>}
+        />
+      </Stack>
+    </Box>
   )
 }
 
-export default allProperties
+export default AllProperties
