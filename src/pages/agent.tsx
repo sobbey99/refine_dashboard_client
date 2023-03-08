892b0b7f7561f29a5c9fc1005f76d43790c6e108
sobbey99 <sobbey99@gmail.com>
2023-03-08 22:51:28 +0200

    AgentCard

--- src/pages/agent.tsx
+++ src/pages/agent.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { useList } from "@pankod/refine-core"
+import {Box, Typography} from "@pankod/refine-mui"
 
-const agent = () => {
+import { AgentCard } from "components"
+
+const Agents = () => {
+  const {data, isLoading, isError} = useList({
+    resource: "users"
+  })
+
+  const allAgents = data?.data ?? []
+
+  if(isLoading) return <div>Loading...</div>
+  if(isError) return <div>Error!</div>
   return (
-    <div>agent</div>
+    <Box>
+      <Typography 
+      fontSize={25} 
+      fontWeight={700}
+      color="#11142d">Agents List</Typography>
+
+      <Box
+      mt="20px"
+      sx={{
+        display:"flex",
+        flexWrap: "wrap",
+        gap: "20px",
+        backgroundColor: "#fcfcfc"
+      }}
+      >
+          {allAgents.map(agent => (
+            <AgentCard 
+              key={agent._id}
+              id={agent._id}
+              name={agent.name}
+              email={agent.email}
+              avatar={agent.avatar}
+              noOfProperties={agent.allProperties.length}
+            />
+          ))}
+      </Box>
+    </Box>
   )
 }
 
-export default agent
+export default Agents
