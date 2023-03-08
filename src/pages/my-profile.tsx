b04137894f6a24d0bce1632ba4398d396d59850b
sobbey99 <sobbey99@gmail.com>
2023-03-08 23:13:49 +0200

    My Profile

--- src/pages/my-profile.tsx
+++ src/pages/my-profile.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useGetIdentity, useOne } from "@pankod/refine-core";
 
-const myProfile = () => {
-  return (
-    <div>my-profile</div>
-  )
-}
+import { Profile } from "components";
 
-export default myProfile
+const MyProfile = () => {
+    const { data: user } = useGetIdentity();
+    console.log(user)
+    const { data, isLoading, isError } = useOne({
+        resource: "users",
+        id: user?.userId,
+    });
+
+    const myProfile = data?.data ?? [];
+
+    if (isLoading) return <div>loading...</div>;
+    if (isError) return <div>error...</div>;
+
+    return (
+        <Profile
+            type="My"
+            name={myProfile.name}
+            email={myProfile.email}
+            avatar={myProfile.avatar}
+            properties={myProfile.allProperties}
+        />
+    );
+};
+
+export default MyProfile;
